f0f125f289b2471726f80e55ab7a8e2955c9172f
Daniel Brígida <danielbrigida@MacBook-Air-de-Daniel.local>
2025-09-15 17:17:05 -0300

    feat: update readme

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,17 @@
+'use client';
+import { RootState } from '@/store';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { useSelector } from 'react-redux';
+
 export default function Home() {
+  const navigation = useRouter();
+  const token = useSelector((state: RootState) => state.auth.token);
+  useEffect(() => {
+    if (!token) navigation.push('/login');
+
+    navigation.push('/products');
+  }, [token]);
   return (
     <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20"></div>
   );
